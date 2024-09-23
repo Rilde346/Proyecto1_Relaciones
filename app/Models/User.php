6672eed3544108cc63fc48b82371450d957dc8b2
Ilderios <rilde346@gmail.com>
2024-09-22 19:00:56 -0600

    Add models, factories, and migrations for new entities
    - Added models for Category, Comment, Group, Image, Level, Location, Post, Profile, Tag, User, and Video
    - Created corresponding factories and migrations
    - Updated User model and UserFactory
    - Modified AppServiceProvider, providers.php, composer.json, composer.lock
    - Updated views and routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,29 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class)->withTimestamps();
+    }
+    public function location()
+    {
+        return $this->hasOneThrough(Location::class, Profile::class);
+    }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
